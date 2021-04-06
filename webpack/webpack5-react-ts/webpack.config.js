d58ff755262a951f21749536c6505c508aa2e105
Yingjie Guo <yguo@appen.com>
2021-04-06 19:04:29 +0800

    React empty project with webpack5 and typescript

---
+++ webpack/webpack5-react-ts/webpack.config.js
@@ -0,0 +1,87 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // generate index.html from template
+const InterpolateHtmlPlugin = require("interpolate-html-plugin"); // inject custom variables to html template file
+const CopyWebpackPlugin = require("copy-webpack-plugin"); // copy public folder to dist
+
+module.exports = {
+  entry: "./src/App.tsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  mode: "development",
+
+  optimization: {
+    usedExports: true,
+  },
+
+  devServer: {
+    contentBase: "./dist",
+    hot: true,
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "cheap-module-source-map",
+
+  performance: {
+    hints: "warning",
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+      //   minify: {
+      //     removeComments: true,
+      //     collapseWhitespace: true,
+      //     removeRedundantAttributes: true,
+      //     useShortDoctype: true,
+      //     removeEmptyAttributes: true,
+      //     removeStyleLinkTypeAttributes: true,
+      //     keepClosingSlash: true,
+      //     minifyJS: true,
+      //     minifyCSS: true,
+      //     minifyURLs: true,
+      //   },
+    }),
+    new InterpolateHtmlPlugin({
+      PUBLIC_URL: "public",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public"),
+          to: path.resolve(__dirname, "dist", "public"),
+        },
+      ],
+    }),
+  ],
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        // this loader will handle svg file in react component
+        test: /\.svg$/,
+        loader: "svg-url-loader",
+      },
+    ],
+  },
+};
